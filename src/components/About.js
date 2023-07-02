f410adff7eff990db28f535aaddd44323176d378
Ceren Şahin <cerennssahinn@gmail.com>
2023-07-02 09:04:09 +0300

    Implemented Links as function, 1 test failed, completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,33 @@
 import React from "react";
 
-function About() {
+
+function About(props) {
+  // console.log(props);
+  function validBio() {
+    if (props.bio && props.bio.length > 0) {
+      return <p>{props.bio}</p>;
+    }
+    else {
+      return null;
+    }
+  }
+  
+  
+  function links() {
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={props.links.github}>{props.links.github}</a>
+            <a href={props.links.linkedin}>{props.links.linkedin}</a>
+        </div>
+    )
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {validBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+       {links()}
     </div>
   );
 }
